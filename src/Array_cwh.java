1623f165019ff15c66e58db80e7f53919d212687
Hemant <hemantchaudhary4321@gmail.com>
2022-10-01 21:51:32 +0530

    sdg

---
+++ src/Array_cwh.java
@@ -0,0 +1,9 @@
+public class Array_cwh
+{
+    public static void main(String[] args) {
+        int [] marks = {23,45,67,32,9};
+        for (int  element: marks){
+            System.out.println(element);
+        }
+    }
+}
